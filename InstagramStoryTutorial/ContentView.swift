12d711347c0296df789aae810ae7a171fa8a2b0c
Jean-Marc Boullianne <jboullianne@gmail.com>
2020-04-14 12:48:04 -0500

    Initial Commit

---
+++ InstagramStoryTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramStoryTutorial
+//
+//  Created by Jean-Marc Boullianne on 4/14/20.
+//  Copyright © 2020 TrailingClosure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
